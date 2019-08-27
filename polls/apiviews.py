db50e3cb3bfa4b439caf591a18e47e921c790289
pratikroy <pratikroy2206@gmail.com>
2019-08-27 22:01:30 +0530

    Added and tested functionality in both generics and viewsets

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,5 +1,7 @@
-#from rest_framework.views import APIView
-#from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
 #from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from polls import models
@@ -33,11 +35,32 @@ class PollDetail(generics.RetrieveDestroyAPIView):
     queryset = models.Poll.objects.all()
     serializer_class = serializers.PollSerializer
 
+class PollViewSet(viewsets.ModelViewSet):
+    """Merge the above two functionality into one"""
+    queryset = models.Poll.objects.all()
+    serializer_class = serializers.PollSerializer
+
 class ChoiceList(generics.ListCreateAPIView):
     """ListCreateAPIView(allowed methods) - GET, POST"""
-    queryset = models.Choice.objects.all()
+    def get_queryset(self):
+        queryset = models.Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
     serializer_class = serializers.ChoiceSerializer
 
-class CreateVote(generics.CreateAPIView):
-    """CreateAPIView(allowed methods) - POST"""
+class CreateVote(APIView):
+    """Customized API according to your need"""
     serializer_class = serializers.VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {
+            "choice":choice_pk,
+            "poll":pk,
+            "voted_by":voted_by
+        }
+        serializer = serializers.VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path, include
 from polls import apiviews
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("pollset", apiviews.PollViewSet, base_name="pollset")
+
 
 urlpatterns = [
     path("polls/", apiviews.PollList.as_view(), name="polls_list"),
     path("polls/<int:pk>/", apiviews.PollDetail.as_view(), name="poll_detail"),
-    path("choices/", apiviews.ChoiceList.as_view(), name="choice_list"),
-    path("vote/", apiviews.CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/",
+        apiviews.ChoiceList.as_view(),
+        name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",
+        apiviews.CreateVote.as_view(),
+        name="create_vote"),
 ]
+
+urlpatterns += router.urls
