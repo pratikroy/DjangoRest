4283e8add6c1b07328665de572abd130995b1c35
pratikroy <pratikroy2206@gmail.com>
2019-08-27 19:49:52 +0530

    API end points tested with DRF generics views

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,20 +1,43 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-
+#from rest_framework.views import APIView
+#from rest_framework.response import Response
+#from django.shortcuts import get_object_or_404
+from rest_framework import generics
 from polls import models
 from polls import serializers
 
-class PollList(APIView):
-     def get(self, request):
-         """Get the list of first 20 Polls"""
-         polls = models.Poll.objects.all()[:20]
-         data = serializers.PollSerializer(polls, many=True).data
-         return Response(data)
+# Example codes given below are written with APIView. Now we are goint to
+# achieve the same using DRF generic views. So comment the following codes
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        """Get detail of a Poll"""
-        poll = get_object_or_404(models.Poll, pk=pk)
-        data = serializers.PollSerializer(poll).data
-        return Response(data)
+# class PollList(APIView):
+#      def get(self, request):
+#          """Get the list of first 20 Polls"""
+#          polls = models.Poll.objects.all()[:20]
+#          data = serializers.PollSerializer(polls, many=True).data
+#          return Response(data)
+#
+# class PollDetail(APIView):
+#     def get(self, request, pk):
+#         """Get detail of a Poll"""
+#         poll = get_object_or_404(models.Poll, pk=pk)
+#         data = serializers.PollSerializer(poll).data
+#         return Response(data)
+
+# Example codes with DRF generics views
+class PollList(generics.ListCreateAPIView):
+    """ListCreateAPIView(allowed methods) - GET, POST"""
+    queryset = models.Poll.objects.all()
+    serializer_class = serializers.PollSerializer
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    """RetrieveDestroyAPIView(allowed methods) - GET, DELETE"""
+    queryset = models.Poll.objects.all()
+    serializer_class = serializers.PollSerializer
+
+class ChoiceList(generics.ListCreateAPIView):
+    """ListCreateAPIView(allowed methods) - GET, POST"""
+    queryset = models.Choice.objects.all()
+    serializer_class = serializers.ChoiceSerializer
+
+class CreateVote(generics.CreateAPIView):
+    """CreateAPIView(allowed methods) - POST"""
+    serializer_class = serializers.VoteSerializer
--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from polls import apiviews
 urlpatterns = [
     path("polls/", apiviews.PollList.as_view(), name="polls_list"),
     path("polls/<int:pk>/", apiviews.PollDetail.as_view(), name="poll_detail"),
+    path("choices/", apiviews.ChoiceList.as_view(), name="choice_list"),
+    path("vote/", apiviews.CreateVote.as_view(), name="create_vote"),
 ]
